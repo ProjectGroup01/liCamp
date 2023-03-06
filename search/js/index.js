0b9fd42471789b8e06e464e74249b599b03acea2
ProjectGroup01 <127112870+ProjectGroup01@users.noreply.github.com>
2023-03-06 22:15:04 +0900

    Make Project PageFiles

---
+++ search/js/index.js
@@ -0,0 +1,10 @@
+jQuery(function($) {
+//     let bWidth = window.innerWidth;
+// window.addEventListener("resize", () => {
+// const nWidth = window.innerWidth;
+// if ((bWidth < 1024 && nWidth >= 1024) || (bWidth > 1023 && nWidth <= 1023)) {
+// location.reload();
+// }
+// beforeWidth = nowWidth;
+// });
+});
